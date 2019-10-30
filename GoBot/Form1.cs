3e36db42c5eabab823b13ec9924eed513da1573c
Andrew Tanner <astanner2000@gmail.com>
2019-10-30 12:59:31 -0400

    added input frameworks

--- GoBot/Form1.cs
+++ GoBot/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,128 @@ namespace GoBot
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Send(ScanCodeShort a)
+        {
+            INPUT[] Inputs = new INPUT[1];
+            INPUT Input = new INPUT();
+            Input.type = 1; // 1 = Keyboard Input
+            Input.U.ki.wScan = a;
+            Input.U.ki.dwFlags = KEYEVENTF.SCANCODE;
+            Inputs[0] = Input;
+            SendInput(1, Inputs, INPUT.Size);
+        }
+
+
+
+
+
+
+
+
+
+
+        /**
+         * Begin declaration of input overrides. DO NOT TOUCH
+         */
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(
+            uint nInputs,
+            [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs,
+            int cbSize);
+
+
+        // Declare the INPUT struct
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INPUT
+        {
+            public uint type;
+            public InputUnion U;
+            public static int Size
+            {
+                get { return Marshal.SizeOf(typeof(INPUT)); }
+            }
+        }
+
+        // Declare the InputUnion struct
+        [StructLayout(LayoutKind.Explicit)]
+        public struct InputUnion
+        {
+            [FieldOffset(0)]
+            internal MOUSEINPUT mi;
+            [FieldOffset(0)]
+            internal KEYBDINPUT ki;
+            [FieldOffset(0)]
+            internal HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            internal int dx;
+            internal int dy;
+            internal MouseEventDataXButtons mouseData;
+            internal MOUSEEVENTF dwFlags;
+            internal uint time;
+            internal UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        public enum MouseEventDataXButtons : uint
+        {
+            Nothing = 0x00000000,
+            XBUTTON1 = 0x00000001,
+            XBUTTON2 = 0x00000002
+        }
+
+        [Flags]
+        public enum MOUSEEVENTF : uint
+        {
+            ABSOLUTE = 0x8000,
+            HWHEEL = 0x01000,
+            MOVE = 0x0001,
+            MOVE_NOCOALESCE = 0x2000,
+            LEFTDOWN = 0x0002,
+            LEFTUP = 0x0004,
+            RIGHTDOWN = 0x0008,
+            RIGHTUP = 0x0010,
+            MIDDLEDOWN = 0x0020,
+            MIDDLEUP = 0x0040,
+            VIRTUALDESK = 0x4000,
+            WHEEL = 0x0800,
+            XDOWN = 0x0080,
+            XUP = 0x0100
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KEYBDINPUT
+        {
+            internal VirtualKeyShort wVk;
+            internal ScanCodeShort wScan;
+            internal KEYEVENTF dwFlags;
+            internal int time;
+            internal UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        public enum KEYEVENTF : uint
+        {
+            EXTENDEDKEY = 0x0001,
+            KEYUP = 0x0002,
+            SCANCODE = 0x0008,
+            UNICODE = 0x0004
+        }        
+
+        /// <summary>
+        /// Define HARDWAREINPUT struct
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HARDWAREINPUT
+        {
+            internal int uMsg;
+            internal short wParamL;
+            internal short wParamH;
         }
     }
 }
